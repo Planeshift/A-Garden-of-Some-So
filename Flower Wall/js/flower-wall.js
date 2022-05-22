9049c902fc163909e470f2b00297965450369799
Planeshift <qgarcia09@gmail.com>
2022-05-22 23:32:41 +0200

    Added: Animations on flowers on spawn, some CSS

--- Flower Wall/js/flower-wall.js
+++ Flower Wall/js/flower-wall.js
@@ -1,6 +1,9 @@
 let appContainer = document.getElementById("app-container");
 
 appContainer.addEventListener("click", e => createFlower(e));
+
+let currentFlowers = [];
+
 /**
  * 
  * @param {Event} e 
@@ -26,38 +29,42 @@ function createFlower(e){
         petalColors.reverse();
     }
 
+    // Add to use a wrapper on the flower because, whenever an element is undergoing a transform effect, its zIndex is considered 0 (more ore less), which meant new flowers appeared underneath the old ones, then popped above once the animation finished. 
+    let flowerBox = document.createElement("div");
+    flowerBox.style.position = "absolute";
+    flowerBox.style.top = `${offsetY}px`;
+    flowerBox.style.left = `${offsetX}px`;
+    flowerBox.style.height = `${flowerSize}px`;
+    flowerBox.style.width = `${flowerSize}px`;
+    flowerBox.style.transform = `translate(-50%, -50%) scale(${flowerScale})`;
+    appContainer.appendChild(flowerBox);
+    
     let flower = document.createElement("div");
-    flower.classList.add("flower");
-    appContainer.appendChild(flower);
-    flower.style.top = `${offsetY}px`;
-    flower.style.left = `${offsetX}px`;
-    flower.style.height = `${flowerSize}px`;
-    flower.style.width = `${flowerSize}px`;
-    flower.style.transform = `translate(-50%, -50%) scale(${flowerScale})`;
+    flowerBox.appendChild(flower);
+    flower.style.height = `100%`;
+    flower.style.width = `100%`;
+    flower.style.position = `relative`;
 
-    let pistil = document.createElement("div");
-    pistil.classList.add("pistil");
-    pistil.style.zIndex = crownNumber + 1;
-    pistil.style.height = `${2 * pistilRadius}px`;
-    pistil.style.width = `${2 * pistilRadius}px`;
-    pistil.style.backgroundColor = pistilBackgroundColor;
-    flower.appendChild(pistil);
+    let rotationBaseDirection = Math.random() >= 0.5 ? 1 : -1;
 
+    
     let pistilBorder = document.createElement("div");
     pistilBorder.classList.add("pistil-border");
-    pistilBorder.style.height = `${3 * pistilRadius}px`; // Could not work if petals were different in style (length, width, etc), this is a bit of a magic number situation
-    pistilBorder.style.width = `${3 * pistilRadius}px`;
+    pistilBorder.style.height = `${2.8 * pistilRadius}px`; // Could not work if petals were different in style (length, width, etc), this is a bit of a magic number situation
+    pistilBorder.style.width = `${2.8 * pistilRadius}px`;
     flower.appendChild(pistilBorder);
 
-    let crowns = [];
+    let crowns = document.createElement("div");
+    crowns.style.width = "100%";
+    crowns.style.height = "100%";
+    flower.appendChild(crowns);
 
     for(let i = 0; i < crownNumber; i++ ){
 
         let crown = document.createElement("div");
         crown.classList.add("crown");
-        crown.style.transform = `scale(${1 - (crownNumber - 1 - i) * ( (1 - minCrownScaleSize) / crownNumber)})`;
         crown.style.zIndex = crownNumber - i;
-        flower.appendChild(crown);
+        crowns.appendChild(crown);
 
         for(let j = 0; j < petalNumber; j++){
 
@@ -80,11 +87,71 @@ function createFlower(e){
 
         }
         
-        crowns.push(crown);
+        crown.animate(
+            [
+                { transform: `scale(0) rotate(${rotationBaseDirection * 360}deg)`, offset: 0},
+                { transform: `scale(${ 1.4 * (1 - (crownNumber - 1 - i) * ( (1 - minCrownScaleSize) / crownNumber)) }) rotate(${rotationBaseDirection * 0.2 * 360}deg)`, offset: 0.8},
+                { transform:  `scale(${1 - (crownNumber - 1 - i) * ( (1 - minCrownScaleSize) / crownNumber)}) rotate(0deg)`, offset: 1}
+            ],
+            {
+                fill: "forwards",
+                duration: 1000,
+                easing: "ease-out",
+                iterations: 1
+            }
+        )
+
+        rotationBaseDirection = -rotationBaseDirection;
     }
 
+    let pistil = document.createElement("div");
+    pistil.classList.add("pistil");
+    pistil.style.zIndex = crownNumber + 1;
+    pistil.style.height = `${2 * pistilRadius}px`;
+    pistil.style.width = `${2 * pistilRadius}px`;
+    pistil.style.backgroundColor = pistilBackgroundColor;
+    flower.appendChild(pistil);
 
+    // This was all once a simple animation on the whole flower, however, this made the pistil go beneath the petals during the animation on Chrome.
 
+    pistil.animate(
+        [
+            { transform: `translate(-50%, -50%) scale(0)`},
+            { transform: `translate(-50%, -50%) scale(1)`}
+        ],
+        {
+            duration: 1000,
+            easing: "ease-out",
+            iterations: 1
+        }
+    )
+    
+    pistilBorder.animate(
+        [
+            { transform: `translate(-50%, -50%) scale(0)`},
+            { transform: `translate(-50%, -50%) scale(1)`}
+        ],
+        {
+            duration: 1000,
+            easing: "ease-out",
+            iterations: 1
+        }
+    )
+
+    crowns.animate(
+        [
+            { transform: `scale(0)`},
+            { transform: `scale(1)`}
+        ],
+        {
+            duration: 1000,
+            easing: "ease-out",
+            iterations: 1
+        }
+    )
+
+    currentFlowers.push(flower);
+    
 }
 
 function getPistilColor(){
@@ -96,7 +163,13 @@ function getPistilColor(){
 }
 
 function getPetalColors(n = 1){
-    let h = Math.random() * 360;
+    let h;
+
+    // Prevent colors from being too close to our green background, or too yellow
+    do{
+        h = Math.random() * 360;
+    }while(h <= 140 && h >= 25);
+    
     let s = Math.random() * 10 + 50;
     let l = Math.random() * 10 + 60;
 
